04819c1c7879a97943af21c6a62a6042c72478d5
Ruth Ivimey-Cook <ruth@ivimey.org>
2020-06-27 02:10:34 +0100

    Provide more configuration for spamassassin, antivirus etc, add mailman3 support, disable ipv6 option

---
+++ files/mk-greylist.sql
@@ -0,0 +1,14 @@
+CREATE TABLE resenders (
+        host            TEXT,
+        helo            TEXT,
+        time            INTEGER,
+    PRIMARY KEY (host, helo)
+);
+
+CREATE TABLE greylist (
+        id              TEXT PRIMARY KEY,
+        expire          INTEGER,
+        host            TEXT,
+        helo            TEXT
+);
+
